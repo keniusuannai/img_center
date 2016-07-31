d0bd81a7d2c588d6d0142ea38899126505bf41e0
tage <896932646@qq.com>
2016-07-31 23:14:09 +0800

    init commit

--- mainApp/views.py
+++ mainApp/views.py
@@ -2,18 +2,18 @@ from django.http import HttpResponse
 from PIL import Image, ImageFont, ImageDraw
 
 
-def index(request, width=0, height=0, bg_color='#ccc', fn_color='#969696'):
+def index(request, width=0, height=0, bg_color='ccc', fn_color='969696'):
     _width = int(width)
     _height = int(height)
-    image = Image.new('RGB', (_width, _height), bg_color)
-
-    text = request.GET.get('text', str(width) + '×' + str(height))
-    font = ImageFont.truetype('/static/msyh.ttc', int(_width / 10))
+    txt = request.GET.get('text', str(width) + '×' + str(height)).replace('+', ' ')
+    txtsize = request.GET.get('txtsize', int(_width / 10))
+    image = Image.new('RGB', (_width, _height), '#' + bg_color)
+    font = ImageFont.truetype('/static/msyh.ttc', int(txtsize))
     draw = ImageDraw.Draw(image)
 
-    draw.text(((_width - font.getsize(text)[0]) / 2, (_height - font.getsize(text)[1] - _width / 10) / 2), text,
+    draw.text(((_width - font.getsize(txt)[0]) / 2, (_height - font.getsize(txt)[1] - _width / 10) / 2), txt,
               font=font,
-              fill=fn_color)
+              fill='#' + fn_color)
 
     response = HttpResponse(content_type="image/jpeg")
     image.save(response, "JPEG", quality=100)
