6a6be6c9a6db1053fc01339a456ff7928982dcbe
zhangzexuan <zhangzexuan@4399inc.com>
2016-08-01 16:39:47 +0800

    修改mainApp为placehold

--- img_center/urls.py
+++ img_center/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from mainApp import views as main_views
+from placehold import views as main_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
